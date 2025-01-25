286cdc63418351d2d453db2c601bed7b337451ed
Stefan Fische <Stefan.Fischer@scch.at>
2025-01-25 11:01:37 +0100

    TODO Create evaluation using AGORA data

--- evaluation/build.gradle.kts
+++ evaluation/build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 dependencies {
     implementation(project(":core"))
 
+    implementation("org.javatuples:javatuples:1.2")
+    implementation("org.json:json:20240303")
+
+    implementation("org.apache.commons:commons-csv:1.12.0")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
