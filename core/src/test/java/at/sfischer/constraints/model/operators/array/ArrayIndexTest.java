0f87bcc54e8d03e761561c8cc5383f488665452a
Stefan Fischer <stefan.fischer@scch.at>
2025-05-29 17:03:16 +0200

    Test case for array index in simple data schema

--- core/src/test/java/at/sfischer/constraints/model/operators/array/ArrayIndexTest.java
+++ core/src/test/java/at/sfischer/constraints/model/operators/array/ArrayIndexTest.java
@@ -1,6 +1,11 @@
 package at.sfischer.constraints.model.operators.array;
 
+import at.sfischer.constraints.Constraint;
+import at.sfischer.constraints.ConstraintResults;
+import at.sfischer.constraints.data.*;
 import at.sfischer.constraints.model.*;
+import at.sfischer.constraints.model.operators.numbers.GreaterThanOperator;
+import at.sfischer.constraints.model.operators.numbers.OneOfNumber;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,5 +53,31 @@ public class ArrayIndexTest {
 		Node result = operator.evaluate();
 
 		assertEquals(operator, result);
+	}
+
+	@Test
+	public void applyInSimpleDataSchema() {
+		SimpleDataSchema schema = new SimpleDataSchema();
+
+		DataSchemaEntry<SimpleDataSchema> objectEntry = schema.objectEntry("object", true);
+
+		DataSchemaEntry<SimpleDataSchema> arrayEntry = objectEntry.dataSchema.numberArrayEntry("array", true);
+		DataSchemaEntry<SimpleDataSchema> indexEntry = objectEntry.dataSchema.numberEntry("index", true);
+
+		Constraint idConstraint = new Constraint(new OneOfNumber(new ArrayIndex(new DataReference(arrayEntry), new DataReference(indexEntry)), new NumberLiteral(1)));
+		indexEntry.constraints.add(idConstraint);
+
+		SimpleDataCollection data = SimpleDataCollection.parseData(
+				"{object: {array:[1,2,3], index: 1}}",
+				"{object: {array:[1,0,2], index: 2}}",
+				"{object: {array:[2,1,3], index: 0}}"
+		);
+
+		EvaluationResults<SimpleDataSchema, DataObject> actual = schema.evaluate(data);
+		ConstraintResults<DataObject> results = actual.getConstraintResults(indexEntry, idConstraint, data);
+
+		assertTrue(actual.getEvaluationResults().isEmpty());
+		assertEquals(1.0, results.applicationRate());
+		assertFalse(results.foundCounterExample());
 	}
 }
